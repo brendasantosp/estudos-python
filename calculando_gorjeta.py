eb452287daf24efba2cc9383582a7213ffb6c965
brenda-santos-pereira <brenda.jspereira@gmail.com>
2026-08-27 13:41:25 -0300

    projeto calcular uma gorjeta

---
+++ calculando_gorjeta.py
@@ -0,0 +1,8 @@
+valor_conta = float(input('Digite o valor da conta: '))
+porcentagem_gorjeta = float(input('Digite a porcentagem da gorjeta: '))
+
+gorjeta = valor_conta * (porcentagem_gorjeta / 100) * valor_conta
+total_a_pagar = valor_conta + gorjeta
+print(f"Valor da gorjeta: R$ {gorjeta:.2f}")
+print(f"Total a pagar: R$ {total_a_pagar:.2f}")
+
